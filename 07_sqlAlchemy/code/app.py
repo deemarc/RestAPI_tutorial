f8d0f5eb195f16100e14e6743bf5644b04298421
Burakitbumrung <Deemarc.Burakitbumrung@thomsonreuters.com>
2019-05-27 18:36:24 +0700

    now item is interface through sqlAlchemy

--- 07_sqlAlchemy/code/app.py
+++ 07_sqlAlchemy/code/app.py
@@ -7,6 +7,9 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 app.secret_key = 'jose' #require for jwt
 api = Api(app)
 
@@ -19,4 +22,7 @@ api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList,'/items')
 api.add_resource(UserRegister,'/register')
 
-app.run(port=5000, debug=True)
+if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
+    app.run(port=5000, debug=True)
